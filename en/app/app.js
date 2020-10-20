0471b506fbb82da04697d0f3d33c30a9dd3b4344
potamon17 <tancorov.andriy@gmail.com>
2020-10-20 14:30:30 +0300

    New translations app.js (English)

--- en/app/app.js
+++ en/app/app.js
@@ -1,13 +1,25 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-// require('./bootstrap');
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-Vue.component('example', require('./components/Example.vue'));
-const app = new Vue({ el: '#app' });
+$(document).ready(function () {
+    // responsive nav
+    var responsiveNav = $('#toggle-nav');
+    var navBar = $('.nav-bar');
+    responsiveNav.on('click', function (e) {
+        e.preventDefault();
+        console.log(navBar);
+        navBar.toggleClass('active');
+    });
+    // pseudo active
+    if ($('#docs').length) {
+        var sidenav = $('ul.side-nav').find('a');
+        var url = window.location.pathname.split('/');
+        var url = url[url.length - 1];
+        sidenav.each(function (i, e) {
+            var active = $(e).attr('href');
+            if (active === url) {
+                $(e).parent('li').addClass('active');
+                return false;
+            }
+        });
+    }
+});
+hljs.configure({ tabReplace: '  ' });
+hljs.initHighlightingOnLoad();
